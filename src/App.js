aa013019d20e15b722d889e0b3d4f1e17ae6f5a5
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2022-03-07 22:15:42 -0300

    styling page

--- src/App.js
+++ src/App.js
@@ -14,23 +14,37 @@ componentDidMount() {
 
 loadPosts = async () => {
   const postsResponse = fetch ('https://jsonplaceholder.typicode.com/posts');
-  const [posts] = await Promise.all([postsResponse]);
-  const postsjson = await posts.json();
-  this.setState({posts : postsjson});
+  const photosResponse = fetch ('https://jsonplaceholder.typicode.com/photos')
+
+  const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+  const postsJson = await posts.json();
+  const photosJson = await photos.json();
+
+  const postsAndPhotos = postsJson.map((post, indice) => {
+    return { ...post, cover: photosJson[indice].url }
+  });
+
+  this.setState({posts : postsAndPhotos});
 }
 
   render() {
     const { posts } = this.state;
 
     return (
-      <div className="App">
+      <section className='container'>
+        <div className="posts">
           {posts.map(post => (
-            <div key={post.id}>
-              <h1>{post.title}</h1>
-              <p>{post.body}</p>
+            <div className='post'>
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className="post-content">
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
             </div>
           ))}
       </div>
+      </section>
     );
   }
 }
