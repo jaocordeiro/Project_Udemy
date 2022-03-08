3de3e217ee4131a052af0de69e21570fd9aa5b7c
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2022-03-07 21:33:07 -0300

    clear code and data fetching API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    posts: []
+  };
+
+componentDidMount() {
+  this.loadPosts();
+}
+
+loadPosts = async () => {
+  const postsResponse = fetch ('https://jsonplaceholder.typicode.com/posts');
+  const [posts] = await Promise.all([postsResponse]);
+  const postsjson = await posts.json();
+  this.setState({posts : postsjson});
+}
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <div className="App">
+          {posts.map(post => (
+            <div key={post.id}>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          ))}
+      </div>
+    );
+  }
 }
 
 export default App;
