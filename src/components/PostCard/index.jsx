2097f9678d1bf119140c8120c1e64d0600c69978
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2022-04-04 21:41:43 -0300

    application test start - pt2

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -6,7 +6,7 @@ export const PostCard = ({cover, title, id, body}) => {
     <div className='post'>
       <img src={cover} alt={title} />
       <div className="post-content">
-        <h2>{title}</h2>
+        <h2>{title} {id}</h2>
         <p>{body}</p>
       </div>
     </div>
